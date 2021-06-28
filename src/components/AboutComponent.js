06232e48897d9c3f7d96b870aaa2fb00180e5040
Danielle Robitshek <drobi710@gmail.com>
2021-06-28 19:19:01 -0400

    added about me card

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import {
+    Card,
+    CardImg,
+    CardText,
+    CardBody,
+    CardTitle
+  } from "reactstrap";
+
+  import MyPic from "../assets/images/portfolio/linkedinimg.jpg"
 
 function About(props) {
     return (
-        <div className="container">
-            <h4>All About Me!</h4>
-        </div>
+        <Card className="w-75 align-items-center mr-auto ml-auto p-5 m-5">
+        <CardTitle className="">Danielle Robitshek</CardTitle>
+        <CardImg className='w-50' top src={MyPic} alt='picture of Danielle Robitshek' />
+        <CardBody>
+            <CardText className=''>Hello World!</CardText>
+        </CardBody>
+    </Card>
     );
 }
 
