af173cee306c1780c91c1a74e0473448d7c5ae10
Danielle Robitshek <drobi710@gmail.com>
2021-07-21 22:21:38 -0400

    add image carousel to Nucampsite using React Hooks and updatie project info

--- src/components/NucampsiteComponent.js
+++ src/components/NucampsiteComponent.js
@@ -8,44 +8,53 @@ import {
   CardTitle,
   CardText,
 } from "reactstrap";
+import ImageSlider from "./ImageSliderComponent";
+import { SliderData } from './SliderData';
 import { NavLink } from "react-router-dom";
 
 function Nucampsite(props) {
-    return (
-        <div className="col-11 col-md-10 m-auto">
-          <Card className="mt-4">
-            <CardTitle className="mt-4 textTitle">Nucamping</CardTitle>
-            <hr />
-            <CardBody>
-              {/* <CardImg
-                className="w-75"
-                id="projectPic"
-                src={project.image}
-                alt={project.name}
-              /> */}
-              <CardText className="mt-4 px-5 textSubB">
-              Reactive Native App for campground sites
-              </CardText>
-              <CardText className="textSub">Git, GitHub, Yarn, Visual Studio Code, React Native, React Native Elements, Redux, Modals, Form Validation, Stack Navigation, Drawer Navigation Expo SDK, Gestures, Animatable, DateTimePicker, Alert API </CardText>
-              <CardText className="textContent mx-4">
-              A Reactive Native App for campground sites with user authentication, that allows users to make a profile and upload an image, search for campsite availability, see descriptions about different campsites, leave comments about the campsites they visit and pick favorites to save.
-              </CardText>
-              <ButtonGroup id="socialnav" className="my-3">
-                <a href="https://github.com/drobi710/Nucampsite-React-Native" rel="noopener noreferrer" target="_blank">
-                  <Button id="gitProjBtn" className="my-4 px-2">
-                    <i class="fa fa-github fa-lg px-5" />
-                  </Button>
-                </a>
-                <a href="/soon" rel="noopener noreferrer" target="_blank">
-                  <Button id="webProjBtn" className="my-4 px-2">
-                    <i class="fa fa-desktop fa-lg px-5" />
-                  </Button>
-                </a>
-              </ButtonGroup>
-            </CardBody>
-          </Card>
-        </div>
-    )
+  return (
+    <div className="col-11 col-md-10 m-auto">
+      <Card className="mt-4">
+        <CardTitle className="mt-4 textTitle">Nucamping</CardTitle>
+        <hr />
+        <CardBody>
+        <ImageSlider slides={SliderData} />
+          <CardText className="mt-1 px-5 textSubB">
+            Reactive Native App for campground sites
+          </CardText>
+          <CardText className="textSub">
+            Git, GitHub, Yarn, Visual Studio Code, React Native, React Native
+            Elements, React Hooks, Redux, Modals, Form Validation, Stack Navigation, Drawer
+            Navigation Expo SDK, Gestures, Animatable, DateTimePicker, Alert API{" "}
+          </CardText>
+          <CardText className="textContent mx-4">
+            A Reactive Native App for campground sites with user authentication,
+            that allows users to make a profile and upload an image, search for
+            campsite availability, see descriptions about different campsites,
+            leave comments about the campsites they visit and pick favorites to
+            save.
+          </CardText>
+          <ButtonGroup id="socialnav" className="my-3">
+            <a
+              href="https://github.com/drobi710/Nucampsite-React-Native"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <Button id="gitProjBtn" className="my-4 px-2">
+                <i class="fa fa-github fa-lg px-5" />
+              </Button>
+            </a>
+            <a href="/soon" rel="noopener noreferrer" target="_blank">
+              <Button id="webProjBtn" className="my-4 px-2">
+                <i class="fa fa-desktop fa-lg px-5" />
+              </Button>
+            </a>
+          </ButtonGroup>
+        </CardBody>
+      </Card>
+    </div>
+  );
 }
 
 export default Nucampsite;
---
+++ src/components/SliderData.js
@@ -0,0 +1,33 @@
+import nC1 from "../assets/images/nC1.PNG";
+import nC2 from "../assets/images/nC2.PNG";
+import nC3 from "../assets/images/nC3.gif";
+import nC4 from "../assets/images/nC4.PNG";
+import nC5 from "../assets/images/nC5.PNG";
+import nC6 from "../assets/images/nC6.PNG";
+
+export const SliderData = [
+    {
+      image:
+        nC1
+    },
+    {
+      image:
+        nC2
+    },
+    {
+      image:
+        nC3
+    },
+    {
+      image:
+      nC4
+    },
+    {
+      image:
+      nC5
+    },
+    {
+      image:
+      nC6
+    }
+  ];
